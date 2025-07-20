f247453f000e35a63e5d258ed68bf8bf723d8db1
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-20 09:38:10 -0300

    exemplos para adicionar no material da aula

---
+++ recursive_tests/Program.cs
@@ -0,0 +1,22 @@
+﻿//fatorial example
+int valor = 5;
+
+int resultado = Fatorial.Result(valor);
+Console.WriteLine(resultado);
+
+
+
+Console.WriteLine(Fatorial.Result(3));
+
+Console.WriteLine(Fatorial.Result(10));
+
+public static class Fatorial 
+{
+    public static int Result(int valor) 
+    {
+        if(valor == 1)
+            return 1;
+        return valor * Result(valor - 1);
+    }
+
+}
