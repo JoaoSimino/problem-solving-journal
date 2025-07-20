fc6d16ec54d176fa703a37d615c1f8e389820f79
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-20 10:54:08 -0300

    implementando e testando algoritmo de ordenacao_quick_sort em c#!

---
+++ ordenacao_quick_sort/Program.cs
@@ -0,0 +1,42 @@
+﻿//com conceitos aprendidos de DC como base, dividir para conquistar!
+
+int[] array = { 9, 3, 7, 1, 8, 2, 5 };
+
+var result = QuickSort(array);
+
+foreach (var item in result) 
+{
+    Console.WriteLine(item);
+}
+
+int[] QuickSort(int[] array) 
+{
+    List<int> maiores = new();
+    List<int> menores = new();
+
+    //condicao de parada..., array com tamanho de um nao precisa ser ordenado !!
+    if (array.Length <= 1)
+        return array;
+
+    //selecionando o primeiro item do array como pivo
+    int pivo = array[0];
+
+    for (int i = 1; i < array.Length ; i++)
+    {
+        if (array[i] > pivo)
+        {
+            maiores.Add(array[i]);
+        }
+        else 
+        {
+            menores.Add(array[i]);
+        }
+    }
+
+    int[] resultado = QuickSort(menores.ToArray())// pulo do gato de escrita do algoritmo!!
+        .Concat(new int[] { pivo })
+        .Concat(QuickSort(maiores.ToArray()))
+        .ToArray();
+
+    return resultado;
+}
