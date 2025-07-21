84fd1da1827f0731563ebd2eed546e8bb0d4e034
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-21 17:59:18 -0300

    resolucao de exercicio, criando orderBy estilo linq para classe personalizada

---
+++ exercicio_quick_sort/Produto.cs
@@ -0,0 +1,46 @@
+﻿namespace Base_Folder1;
+
+class Produto
+{
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+
+
+    //utilizar generic na implementacao
+    //m => m.Coluna  func<T,C>
+    public static List<T> OrdenaPorColuna<T, C>(List<T> lista, Func<T, C> selector)
+        where C : IComparable<C>
+    {
+
+
+        //caso de quebra de loop, caso base onde nao precisa ser ordenado e nao chama mais recursivamente!
+        if (lista.Count <= 1) 
+        {
+            return lista;
+        }
+
+
+        List<T> maiores = new List<T>();
+        List<T> menores = new List<T>();
+        T pivo = lista[0];
+        
+        for (int i = 1; i < lista.Count; i++)
+        {
+            var valorItem = selector(lista[i]);
+            var valorPivo = selector(pivo);
+
+            if (valorItem.CompareTo(valorPivo) < 0)
+                menores.Add(lista[i]);
+            else
+                maiores.Add(lista[i]);
+
+        }
+
+        var ordenados = OrdenaPorColuna(menores, selector);
+        ordenados.Add(lista[0]);
+        ordenados.AddRange(OrdenaPorColuna(maiores, selector));
+
+        return ordenados;
+    }
+}
+
---
+++ exercicio_quick_sort/Program.cs
@@ -0,0 +1,34 @@
+﻿using Base_Folder1;
+using System;
+
+var produtos = new List<Produto>
+{
+    new Produto { Nome = "Notebook", Preco = 3500.00m },
+    new Produto { Nome = "Mouse", Preco = 80.00m },
+    new Produto { Nome = "Teclado", Preco = 120.00m },
+    new Produto { Nome = "Monitor", Preco = 900.00m },
+    new Produto { Nome = "HD Externo", Preco = 400.00m }
+};
+
+var ordenados = Produto.OrdenaPorColuna(produtos, p => p.Nome);
+Console.WriteLine("Ordenador por Nome");
+foreach (var produto in ordenados) 
+{
+    
+    Console.WriteLine($"{produto.Nome}-{produto.Preco}");
+    
+}
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine();
+Console.WriteLine();
+
+var ordenados2 = Produto.OrdenaPorColuna(produtos, p => p.Preco);
+Console.WriteLine("Ordenador por preco");
+foreach (var produto in ordenados2) 
+{
+    Console.WriteLine($"{produto.Nome}-{produto.Preco}");
+}
+    
