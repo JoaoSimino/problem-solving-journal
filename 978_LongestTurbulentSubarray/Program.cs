eb6096fbd0dd6324e764b596c887839b19e2ca7e
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-24 12:11:38 -0300

    estudar melhor algoritmo

---
+++ 978_LongestTurbulentSubarray/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+Given an integer array arr, return the length of a maximum size turbulent subarray of arr.
+
+A subarray is turbulent if the comparison sign flips between each adjacent pair of elements in the subarray.
+
+More formally, a subarray [arr[i], arr[i + 1], ..., arr[j]] of arr is said to be turbulent if and only if:
+
+For i <= k < j:
+arr[k] > arr[k + 1] when k is odd, and
+arr[k] < arr[k + 1] when k is even.
+
+Or, for i <= k < j:
+arr[k] > arr[k + 1] when k is even, and
+arr[k] < arr[k + 1] when k is odd.
+ 
+
+Example 1:
+Input: arr = [9,4,2,10,7,8,8,1,9]
+Output: 5
+Explanation: arr[1] > arr[2] < arr[3] > arr[4] < arr[5]
+
+Example 2:
+Input: arr = [4,8,12,16]
+Output: 2
+
+Example 3:
+Input: arr = [100]
+Output: 1
+  
+*/
+
+int[] arr = { 4, 8, 12, 16 };
+
+Console.WriteLine(new Solution().MaxTurbulenceSize(arr) == 2);
+
+public class Solution
+{
+    public int MaxTurbulenceSize(int[] arr)
+    {
+        if (arr.Length == 1) return 1;
+
+        int wind_size = 1;
+        int max_size = 1;
+        int pos_atual = 0;
+
+        while (pos_atual + 1 < arr.Length)
+        {
+            int atual = arr[pos_atual];
+            int next = arr[pos_atual + 1];
+
+            if (atual == next)
+            {
+                wind_size = 1;
+                pos_atual++;
+                continue;
+            }
+
+            wind_size = 2; // Começamos com dois elementos diferentes
+
+            // Expandir janela enquanto for turbulento, eh a solucao mais facil e elegante
+            int i = pos_atual + 1;
+            while (i + 1 < arr.Length && trocouSinal(arr[i - 1], arr[i], arr[i + 1]))
+            {
+                wind_size++;
+                i++;
+            }
+
+            max_size = Math.Max(max_size, wind_size);
+            pos_atual = i;
+        }
+
+        return max_size;
+    }
+
+    bool trocouSinal(int a, int b, int c)
+    {
+        return (a > b && b < c) || (a < b && b > c);
+    }
+}
---
+++ MaxConsecutiveOnesIII/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Example 1:
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+Example 2:
+
+Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+Output: 10
+Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+*/
+
+Console.WriteLine(new Solution().LongestOnes(new int[] { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 },2) == 6);
+
+Console.WriteLine(new Solution().LongestOnes(new int[] { 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 }, 3) == 10);
+
+Console.WriteLine(new Solution().LongestOnes(new int[] { 0, 0, 0, 1 }, 4) == 4);
+
+public class Solution
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        int left = 0, right = 0;
+        int zeros = 0;
+        int maxLen = 0;
+
+        while (right < nums.Length)
+        {
+            if (nums[right] == 0)
+                zeros++;
+
+            while (zeros > k)
+            {
+                if (nums[left] == 0)
+                    zeros--;
+                left++;
+            }
+
+            maxLen = Math.Max(maxLen, right - left + 1);
+            right++;
+        }
+
+        return maxLen;
+    }
+}
